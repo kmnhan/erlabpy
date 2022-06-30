3a6d8c3b5cc44078cd4b8f5056ccacb21d335428
Kimoon Han <98246499+kmnhan@users.noreply.github.com>
2022-06-30 17:18:58 +0900

    feat: add callable models for edge correction

--- erlab/analysis/utilities.py
+++ erlab/analysis/utilities.py
@@ -1,5 +1,6 @@
 import arpes.xarray_extensions
 import lmfit
+import csaps
 import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
@@ -9,19 +10,31 @@ from ..plotting import plot_array, proportional_colorbar
 __all__ = ['correct_with_edge']
 
 
-def correct_with_edge(fmap: xr.DataArray, modelresult: lmfit.model.ModelResult,
-                      plot=False, **improps):
+def correct_with_edge(fmap: xr.DataArray, modelresult,
+                      plot=False, zero_nans=False, shift_coords=True, **improps):
 
     if isinstance(fmap, xr.Dataset):
         fmap = fmap.spectrum
-    edge_quad = modelresult.eval(x=fmap.phi)
-    corrected = fmap.G.shift_by(edge_quad - np.amax(edge_quad), "eV")
+    if isinstance(modelresult, lmfit.model.ModelResult):
+        edge_quad = modelresult.eval(x=fmap.phi)
+    elif callable(modelresult):
+        edge_quad = modelresult(fmap.phi.values)
+    elif isinstance(modelresult, np.ndarray):
+        if len(fmap.phi) != len(modelresult):
+            raise ValueError("incompatible modelresult dimensions")
+        else:
+            edge_quad = modelresult
+    else:
+        raise ValueError("modelresult must be one of "
+                         "lmfit.model.ModelResult, "
+                         "and np.ndarray or a callable")
+        
+    corrected = fmap.G.shift_by(edge_quad, "eV", zero_nans=zero_nans, shift_coords=shift_coords)
 
     if plot is True:
-        modelresult.plot()
         _, axes = plt.subplots(1, 2, layout='constrained', figsize=(10, 5))
 
-        improps.setdefault('cmap', 'twilight')
+        improps.setdefault('cmap', 'copper')
 
         if fmap.ndim > 2:
             avg_dims = list(fmap.dims)[:]
