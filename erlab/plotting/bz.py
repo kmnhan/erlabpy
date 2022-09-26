9820e340584299f5bbf95aea4193e6c47c026304
Kimoon Han <98246499+kmnhan@users.noreply.github.com>
2022-09-26 11:07:33 +0900

    feat: multiple axes input to bz overlay plotter

--- erlab/plotting/bz.py
+++ erlab/plotting/bz.py
@@ -23,8 +23,11 @@ def plot_hex_bz(a=3.54, rotate=0.0, offset=(0.0, 0.0), ax=None, **kwargs):
         kwargs[k] = kwargs.pop(k, kwargs.pop(*v))
     if ax is None:
         ax = plt.gca()
+    if np.iterable(ax):
+        return [plot_hex_bz(a=a, rotate=rotate, offset=offset, ax=x, **kwargs) for x in ax]
 
     poly = RegularPolygon(
         offset, 6, radius=4 * np.pi / a / 3, orientation=np.deg2rad(rotate), **kwargs
     )
     ax.add_patch(poly)
+    return poly
