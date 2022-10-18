3780f23b4b29b271bc4a640e5c187e176f3813b8
Kimoon Han <98246499+kmnhan@users.noreply.github.com>
2022-10-18 16:03:53 +0900

    refactor: remove font refresh macro Just delete cache...

--- erlab/plotting/__init__.py
+++ erlab/plotting/__init__.py
@@ -21,18 +21,6 @@ __all__ = [
 ]
 
 
-def refresh_fonts(silent=True):
-    for path in matplotlib.font_manager.findSystemFonts():
-        try:
-            matplotlib.font_manager.fontManager.addfont(path)
-        except OSError as exc:
-            if not silent:
-                print("Failed to open font file %s: %s", path, exc)
-        except Exception as exc:
-            if not silent:
-                print("Failed to extract font properties from %s: %s", path, exc)
-
-
 def load_igor_ct(file, name):
     file = pkgutil.get_data(__package__, "IgorCT/" + file)
     cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
@@ -42,7 +30,6 @@ def load_igor_ct(file, name):
     matplotlib.colormaps.register(cmap.reversed())
 
 
-refresh_fonts()
 load_igor_ct("Blue-White.txt", "BuWh")
 
 matplotlib.style.core.USER_LIBRARY_PATHS.append(
