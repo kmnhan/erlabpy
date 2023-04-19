26789677f4fb489af39edcab5b0f73714c1e52b4
Kimoon Han <98246499+kmnhan@users.noreply.github.com>
2023-04-19 20:46:09 +0900

    refactor: syntax cleanup

--- erlab/interactive/imagetool.py
+++ erlab/interactive/imagetool.py
@@ -266,7 +266,7 @@ class ImageTool(QtWidgets.QMainWindow):
         ):
             ax = self.slicer_area.main_image.display_axis[i]
             action.triggered.connect(
-                lambda *_, ax=ax, d=d: self.slicer_area.step_index(ax, d)
+                lambda *, ax=ax, d=d: self.slicer_area.step_index(ax, d)
             )
 
         # multiple cursors
@@ -294,7 +294,7 @@ class ImageTool(QtWidgets.QMainWindow):
         ):
             ax = self.slicer_area.main_image.display_axis[i]
             action.triggered.connect(
-                lambda *_, ax=ax, d=d: self.slicer_area.step_index_all(ax, d)
+                lambda *, ax=ax, d=d: self.slicer_area.step_index_all(ax, d)
             )
 
         ### colormap options
@@ -1355,7 +1355,7 @@ class ItoolPlotItem(pg.PlotItem):
             c.sigDragged.connect(
                 lambda v, line=c, axis=ax: self.line_drag(line, v.temp_value, axis)
             )
-            c.sigClicked.connect(lambda *_, line=c: self.line_click(line))
+            c.sigClicked.connect(lambda *, line=c: self.line_click(line))
 
         if update:
             self.refresh_cursor(new_cursor)
@@ -2347,9 +2347,9 @@ if __name__ == "__main__":
     data = xr.load_dataarray(
         # "~/Documents/ERLab/TiSe2/kxy10.nc"
         # "~/Documents/ERLab/TiSe2/221213_SSRL_BL5-2/fullmap_kconv_.h5"
-        "~/Documents/ERLab/CsV3Sb5/2021_Dec_ALS_CV3Sb5/Data/cvs_kxy_small.nc"
-        # "~/Documents/ERLab/TiSe2/220410_ALS_BL4/map_mm_4d_.nc"
-        ,
+        # "~/Documents/ERLab/CsV3Sb5/2021_Dec_ALS_CV3Sb5/Data/cvs_kxy_small.nc"
+        # "~/Documents/ERLab/CsV3Sb5/2021_Dec_ALS_CV3Sb5/Data/cvs_kxy.nc"
+        "~/Documents/ERLab/TiSe2/220410_ALS_BL4/map_mm_4d_.nc",
         engine="h5netcdf",
     )
     itool(data)
