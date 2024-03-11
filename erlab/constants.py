8e46445efd3fa5fe623e2ab65ee939039b4dc149
Kimoon Han <98246499+kmnhan@users.noreply.github.com>
2024-03-12 02:04:56 +0900

    feat(constants): add electron rest energy mc^2

--- erlab/constants.py
+++ erlab/constants.py
@@ -4,11 +4,17 @@ Physical constants and unit conversion.
 .. currentmodule:: erlab.constants
 
 """
+
 import numpy as np
 
 e: float = 1.602176634e-19  #: Elementary charge :math:`e` (C)
 c: float = 299792458.0  #: Speed of light :math:`c` (m/s)
-m_e: float = 9.1093837015e-31  #: Electron mass :math:`m_e` (kg) (±0.0000000028e-31)
+m_e: float = 9.1093837015e-31  #: Electron mass :math:`m_e` (kg) (±0.000 000 0028e-31)
+
+mcsq: float = (
+    8.1871057769e-14  #: Electron rest energy :math:`m_e c^2` (J) (±0.000 000 0025e-14)
+)
+mcsq_eV: float = 510998.95000  #: Electron rest energy :math:`m_e c^2` (eV) (±0.000 15)
 
 h: float = 6.62607015e-34  #: Planck constant :math:`h` (J·s)
 hc: float = 1.9864458571489286e-25  #: :math:`hc`  (J·m)
@@ -22,7 +28,9 @@ hbar_eV: float = 6.582119569509067e-16  #: Dirac constant :math:`\hbar` (eV·s)
 hbarsq_eV: float = 4.332429802731423e-31  #: :math:`\hbar^2` (eV²·s²)
 
 rel_eV_nm: float = 1239.8419843320028  #: :math:`hc`  (eV·nm)
-rel_kconv: float = 0.512316721967493  #: :math:`\frac{\sqrt{2 m_e}}{\hbar}`, Used in momentum conversion
+rel_kconv: float = (
+    0.512316721967493  #: :math:`\frac{\sqrt{2 m_e}}{\hbar}`, Used in momentum conversion
+)
 rel_kzconv: float = (
     3.8099821161548606  #: :math:`\frac{\hbar^2}{2 m_e}`, Used in momentum conversion
 )
