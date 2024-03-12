429c8681dd90bc3c7c890377eb37f4ad1414c3f7
Kimoon Han <98246499+kmnhan@users.noreply.github.com>
2024-03-12 18:46:24 +0900

    fix: move doc comments to above

--- src/erlab/constants.py
+++ src/erlab/constants.py
@@ -7,36 +7,52 @@ Physical constants and unit conversion.
 
 import numpy as np
 
-e: float = 1.602176634e-19  #: Elementary charge :math:`e` (C)
-c: float = 299792458.0  #: Speed of light :math:`c` (m/s)
-m_e: float = 9.1093837015e-31  #: Electron mass :math:`m_e` (kg) (±0.000 000 0028e-31)
+#: Elementary charge :math:`e` (C)
+e: float = 1.602176634e-19
+#: Speed of light :math:`c` (m/s)
+c: float = 299792458.0
+#: Electron mass :math:`m_e` (kg) (±0.000 000 0028e-31)
+m_e: float = 9.1093837015e-31
 
-mcsq: float = (
-    8.1871057769e-14  #: Electron rest energy :math:`m_e c^2` (J) (±0.000 000 0025e-14)
-)
-mcsq_eV: float = 510998.95000  #: Electron rest energy :math:`m_e c^2` (eV) (±0.000 15)
+#: Electron rest energy :math:`m_e c^2` (J) (±0.000 000 0025e-14)
+mcsq: float = 8.1871057769e-14
 
-h: float = 6.62607015e-34  #: Planck constant :math:`h` (J·s)
-hc: float = 1.9864458571489286e-25  #: :math:`hc`  (J·m)
-hbar: float = 1.0545718176461565e-34  #: Dirac constant :math:`\hbar` (J·s)
-hbarc: float = 3.1615267734966903e-26  #: :math:`\hbar c`  (J·m)
-hbarsq: float = 1.1121217185735183e-68  #: :math:`\hbar^2` (J²·s²)
+#: Electron rest energy :math:`m_e c^2` (eV) (±0.000 15)
+mcsq_eV: float = 510998.95000
 
-h_eV: float = 4.135667696923859e-15  #: Planck constant :math:`h` (eV·s)
-hc_eV: float = 1.2398419843320028e-6  #: :math:`hc`  (eV·m)
-hbar_eV: float = 6.582119569509067e-16  #: Dirac constant :math:`\hbar` (eV·s)
-hbarsq_eV: float = 4.332429802731423e-31  #: :math:`\hbar^2` (eV²·s²)
+#: Planck constant :math:`h` (J·s)
+h: float = 6.62607015e-34
+#: :math:`hc`  (J·m)
+hc: float = 1.9864458571489286e-25
+#: Dirac constant :math:`\hbar` (J·s)
+hbar: float = 1.0545718176461565e-34
+#: :math:`\hbar c`  (J·m)
+hbarc: float = 3.1615267734966903e-26
+#: :math:`\hbar^2` (J²·s²)
+hbarsq: float = 1.1121217185735183e-68
 
-rel_eV_nm: float = 1239.8419843320028  #: :math:`hc`  (eV·nm)
-rel_kconv: float = (
-    0.512316721967493  #: :math:`\frac{\sqrt{2 m_e}}{\hbar}`, Used in momentum conversion
-)
-rel_kzconv: float = (
-    3.8099821161548606  #: :math:`\frac{\hbar^2}{2 m_e}`, Used in momentum conversion
-)
+#: Planck constant :math:`h` (eV·s)
+h_eV: float = 4.135667696923859e-15
+#: :math:`hc`  (eV·m)
+hc_eV: float = 1.2398419843320028e-6
+#: Dirac constant :math:`\hbar` (eV·s)
+hbar_eV: float = 6.582119569509067e-16
+#: :math:`\hbar^2` (eV²·s²)
+hbarsq_eV: float = 4.332429802731423e-31
 
-kb: float = 1.380649e-23  #: Boltzmann constant :math:`k_B` (J/K)
-kb_eV: float = 8.617333262145179e-5  #: Boltzmann constant :math:`k_B` (eV/K)
+#: :math:`hc`  (eV·nm)
+rel_eV_nm: float = 1239.8419843320028
+
+
+#: :math:`\frac{\sqrt{2 m_e}}{\hbar}`, Used in momentum conversion
+rel_kconv: float = 0.512316721967493
+#: :math:`\frac{\hbar^2}{2 m_e}`, Used in momentum conversion
+rel_kzconv: float = 3.8099821161548606
+
+#: Boltzmann constant :math:`k_B` (J/K)
+kb: float = 1.380649e-23
+#: Boltzmann constant :math:`k_B` (eV/K)
+kb_eV: float = 8.617333262145179e-5
 
 
 def conv_eV_nm(value: float) -> float:
