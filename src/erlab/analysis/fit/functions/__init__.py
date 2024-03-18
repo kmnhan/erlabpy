2bc555cfb050d28523f805b75053dcf836759a7f
Kimoon Han <98246499+kmnhan@users.noreply.github.com>
2024-03-18 11:40:19 +0900

    refactor: fit functions submodule

---
+++ src/erlab/analysis/fit/functions/__init__.py
@@ -0,0 +1,11 @@
+from erlab.analysis.fit.functions.general import (
+    TINY,
+    do_convolve,
+    do_convolve_y,
+    gaussian_wh,
+    fermi_dirac,
+    fermi_dirac_linbkg,
+    fermi_dirac_linbkg_broad,
+    step_linbkg_broad,
+    step_broad,
+)
