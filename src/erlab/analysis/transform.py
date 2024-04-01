e0a790833025f0c7e952ad17d120f46de3100555
Kimoon Han <98246499+kmnhan@users.noreply.github.com>
2024-04-01 13:35:17 +0900

    refactor: make rotation transformations try fast interpolator first

--- src/erlab/analysis/transform.py
+++ src/erlab/analysis/transform.py
@@ -6,19 +6,21 @@ import numpy as np
 import xarray as xr
 
 
-def rotateinplane(data: xr.DataArray, rotate):
+def rotateinplane(data: xr.DataArray, rotate, **interp_kwargs):
+    interp_kwargs.setdefault("method", "linearfast")
+
     theta = np.radians(rotate)
     d0, d1 = data.dims
     x = xr.DataArray(data[d0] * np.cos(theta) - data[d1] * np.sin(theta))
     y = xr.DataArray(data[d0] * np.sin(theta) + data[d1] * np.cos(theta))
-    data_r = data.interp({d0: x, d1: y})
-    return data_r
+    return data.interp({d0: x, d1: y}, **interp_kwargs)
 
 
-def rotatestackinplane(data: xr.DataArray, rotate):
+def rotatestackinplane(data: xr.DataArray, rotate, **interp_kwargs):
+    interp_kwargs.setdefault("method", "linearfast")
+
     theta = np.radians(rotate)
     d0, d1, _ = data.dims
     x = xr.DataArray(data[d0] * np.cos(theta) - data[d1] * np.sin(theta))
     y = xr.DataArray(data[d0] * np.sin(theta) + data[d1] * np.cos(theta))
-    data_r = data.interp({d0: x, d1: y})
-    return data_r
+    return data.interp({d0: x, d1: y}, **interp_kwargs)
