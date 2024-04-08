499225149346e970d00b60dcb5ca39af5e5ddb47
Kimoon Han <98246499+kmnhan@users.noreply.github.com>
2024-04-08 17:03:58 +0900

    tests: add tests for fitting functions

---
+++ tests/test_fit_functions_dynamic.py
@@ -0,0 +1,136 @@
+import numpy as np
+import pytest
+import xarray as xr
+
+from erlab.analysis.fit.functions.dynamic import (
+    FermiEdge2dFunction,
+    MultiPeakFunction,
+    PolynomialFunction,
+)
+from erlab.analysis.fit.functions.general import gaussian_wh, lorentzian_wh
+
+RAND_STATE = np.random.RandomState(1)
+
+
+def test_poly_func_call():
+    # Test case 1: Evaluate polynomial function with numpy array input
+    x = np.arange(5, dtype=np.float64)
+    coeffs = RAND_STATE.randn(3)
+    expected_result = np.polyval(np.asarray(list(reversed(coeffs))), x)
+    assert np.allclose(
+        PolynomialFunction(degree=2)(x, *coeffs),
+        expected_result,
+    )
+
+    # Test case 2: Evaluate polynomial function with xarray input
+    x = xr.DataArray(np.arange(5, dtype=np.float64), dims="x")
+    coeffs = RAND_STATE.randn(3)
+    expected_result = np.polyval(np.asarray(list(reversed(coeffs))), x)
+    result = PolynomialFunction(degree=2)(x, *coeffs)
+    assert isinstance(result, xr.DataArray)
+    assert np.allclose(result, expected_result)
+
+    # Test case 3: Evaluate polynomial function with parameters
+    x = np.arange(5, dtype=np.float64)
+    coeffs = RAND_STATE.randn(3)
+    expected_result = np.polyval(np.asarray(list(reversed(coeffs))), x)
+    params = dict(zip([f"c{i}" for i in range(3)], coeffs))
+    result = PolynomialFunction(degree=2)(x, **params)
+    assert np.allclose(result, expected_result)
+
+
+def test_multi_peak_function_call():
+    # Test case 1: Evaluate multi-peak function with numpy array input
+    x = np.linspace(-5, 5, 20, dtype=np.float64)
+    npeaks = 2
+    peak_shapes = ["lorentzian", "gaussian"]
+    fd = False
+    convolve = False
+    params = {
+        "p0_center": 0.0,
+        "p0_height": 1.0,
+        "p0_width": 0.5,
+        "p1_center": 2.0,
+        "p1_height": 0.5,
+        "p1_width": 0.2,
+        "lin_bkg": 0.1,
+        "const_bkg": 0.2,
+    }
+    expected_result = (
+        lorentzian_wh(x, params["p0_center"], params["p0_width"], params["p0_height"])
+        + gaussian_wh(x, params["p1_center"], params["p1_width"], params["p1_height"])
+        + params["lin_bkg"] * x
+        + params["const_bkg"]
+    )
+    assert np.allclose(
+        MultiPeakFunction(npeaks, peak_shapes, fd, convolve)(x, **params),
+        expected_result,
+    )
+    # Test case 2: With different peak shape signature
+    assert np.allclose(
+        MultiPeakFunction(npeaks, "l g", fd, convolve)(x, **params),
+        expected_result,
+    )
+
+    # Test case 3: Evaluate multi-peak function with xarray input
+    x = xr.DataArray(np.linspace(-5, 5, 20, dtype=np.float64), dims="x")
+    result_xr = MultiPeakFunction(npeaks, peak_shapes, fd, convolve)(x, **params)
+    assert isinstance(result_xr, xr.DataArray)
+    assert np.allclose(result_xr, expected_result)
+
+    # Test case 4: Evaluate multi-peak function with different peak shape signatures
+    x = np.linspace(-5, 5, 20, dtype=np.float64)
+    npeaks = 3
+    peak_shapes = ["gaussian", "lorentzian", "gaussian"]
+    fd = True
+    convolve = True
+    params = {
+        "p0_center": 1.0,
+        "p0_height": 0.5,
+        "p0_width": 0.2,
+        "p1_center": 2.0,
+        "p1_height": 1.0,
+        "p1_width": 0.5,
+        "p2_center": 3.0,
+        "p2_height": 0.8,
+        "p2_width": 0.3,
+        "lin_bkg": 0.2,
+        "const_bkg": 0.1,
+        "efermi": 0.0,
+        "temp": 30,
+        "offset": 0.5,
+        "resolution": 0.1,
+    }
+
+    expected_result = MultiPeakFunction(npeaks, "lorentzian", fd, convolve)(x, **params)
+
+    for peak_shapes in ("l l l", ["lorentzian"] * 3):
+        assert np.allclose(
+            MultiPeakFunction(npeaks, peak_shapes, fd, convolve)(x, **params),
+            expected_result,
+        )
+
+
+@pytest.mark.filterwarnings("ignore:overflow encountered in exp.*:RuntimeWarning")
+def test_fermi_edge_2d_function_call():
+    # Difficult to test function values, just check if equal for numpy and xarray
+    eV = np.linspace(-5, 5, 25, dtype=np.float64)
+    alpha = RAND_STATE.randn(20)
+    alpha.sort()
+    params = {
+        "c0": 1.0,
+        "c1": 0.5,
+        "lin_bkg": 0.1,
+        "const_bkg": 0.2,
+        "offset": 0.0,
+        "temp": 30.0,
+        "resolution": 0.02,
+    }
+    out_np = FermiEdge2dFunction()(eV, alpha, **params)
+
+    eV = xr.DataArray(eV, dims="eV")
+    alpha = xr.DataArray(alpha, dims="alpha")
+    out_xr = FermiEdge2dFunction()(eV, alpha, **params)
+
+    assert isinstance(out_xr, xr.DataArray)
+    assert np.allclose(out_np.reshape((25, 20)), out_xr)
---
+++ tests/test_fit_functions_general.py
@@ -0,0 +1,175 @@
+import numpy as np
+import scipy.special
+from erlab.analysis.fit.functions.general import (
+    _gen_kernel,
+    do_convolve,
+    fermi_dirac,
+    fermi_dirac_linbkg,
+    fermi_dirac_linbkg_broad,
+    gaussian_wh,
+    lorentzian_wh,
+    step_broad,
+    step_linbkg_broad,
+)
+import pytest
+
+KB_EV = 8.617333262145179e-5
+
+
+def test_gen_kernel():
+    # Define test input values
+    x = np.array([0, 1, 2, 3, 4])
+    resolution = 2.0
+    pad = 5
+
+    # Call the function to generate the kernel
+    extended, gauss = _gen_kernel(x, resolution, pad)
+
+    # Define the expected results
+    expected_extended = np.arange(-4, 9, 1, dtype=np.float64)
+    expected_gauss = np.array(
+        [
+            7.16733764e-06,
+            9.17419217e-04,
+            2.93574150e-02,
+            2.34859320e-01,
+            4.69718639e-01,
+            2.34859320e-01,
+            2.93574150e-02,
+            9.17419217e-04,
+            7.16733764e-06,
+        ]
+    )
+
+    # Check if the generated extended and gauss arrays match the expected results
+    assert np.allclose(extended, expected_extended)
+    assert np.allclose(gauss, expected_gauss)
+
+
+def test_do_convolve():
+    # Test case 1: Convolve a linear function with a Gaussian kernel
+    x = np.linspace(0, 10, 5)
+
+    def testfunc(x):
+        return x
+
+    resolution = 1.0
+    expected = np.array(
+        [-6.61744490e-24, 5.87148334e00, 1.17429667e01, 1.76144500e01, 2.34859334e01]
+    )
+    result = do_convolve(x, testfunc, resolution)
+    assert np.allclose(result, expected)
+
+    # Test case 2: Convolve a sine function with a Gaussian kernel
+    x = np.linspace(0, 2 * np.pi, 5)
+
+    def testfunc(x):
+        return np.sin(x)
+
+    resolution = 0.5
+    expected = np.array(
+        [0.00000000e00, 2.95132925e00, 3.61433592e-16, -2.95132925e00, -7.22867185e-16]
+    )
+    result = do_convolve(x, testfunc, resolution)
+    assert np.allclose(result, expected)
+
+
+def test_gaussian_wh():
+    x = np.linspace(0, 10, 100)
+    center = 5.0
+    width = 2.0
+    height = 1.0
+    expected_result = height * np.exp(
+        -16 * np.log(2) * (1.0 * x - center) ** 2 / max(1e-10, width**2)
+    )
+    assert np.allclose(gaussian_wh(x, center, width, height), expected_result)
+
+
+def test_lorentzian_wh():
+    x = np.linspace(0, 10, 100)
+    center = 5.0
+    width = 2.0
+    height = 1.0
+    expected_result = height / (1 + 4 * ((1.0 * x - center) / max(1.0e-15, width)) ** 2)
+    assert np.allclose(lorentzian_wh(x, center, width, height), expected_result)
+
+
+@pytest.mark.filterwarnings("ignore:overflow encountered in exp.*:RuntimeWarning")
+def test_fermi_dirac():
+    x = np.linspace(0, 10, 100)
+    center = 5.0
+    temp = 2.0
+    expected_result = 1 / (1 + np.exp((1.0 * x - center) / max(1.0e-15, temp * KB_EV)))
+    assert np.allclose(fermi_dirac(x, center, temp), expected_result)
+
+
+@pytest.mark.filterwarnings("ignore:overflow encountered in exp.*:RuntimeWarning")
+def test_fermi_dirac_linbkg():
+    x = np.linspace(0, 10, 100)
+    center = 5.0
+    temp = 2.0
+    back0 = 1.0
+    back1 = 0.5
+    dos0 = 0.8
+    dos1 = 0.3
+    expected_result = (back0 + back1 * x) + (dos0 - back0 + (dos1 - back1) * x) / (
+        1 + np.exp((1.0 * x - center) / max(1.0e-15, temp * KB_EV))
+    )
+    assert np.allclose(
+        fermi_dirac_linbkg(x, center, temp, back0, back1, dos0, dos1), expected_result
+    )
+
+
+def test_fermi_dirac_linbkg_broad():
+    x = np.linspace(0, 10, 100)
+    center = 5.0
+    temp = 300.0
+    resolution = 0.1
+    back0 = 0.1
+    back1 = 0.2
+    dos0 = 0.3
+    dos1 = 0.4
+    expected_result = do_convolve(
+        x,
+        fermi_dirac_linbkg,
+        resolution=resolution,
+        center=center,
+        temp=temp,
+        back0=back0,
+        back1=back1,
+        dos0=dos0,
+        dos1=dos1,
+    )
+    assert np.allclose(
+        fermi_dirac_linbkg_broad(x, center, temp, resolution, back0, back1, dos0, dos1),
+        expected_result,
+    )
+
+
+def test_step_broad():
+    x = np.linspace(-10, 10, 100)
+    center = 0.0
+    sigma = 1.0
+    amplitude = 1.0
+    expected_result = (
+        amplitude
+        * 0.5
+        * scipy.special.erfc((1.0 * x - center) / max(1.0e-15, np.sqrt(2) * sigma))
+    )
+    assert np.allclose(step_broad(x, center, sigma, amplitude), expected_result)
+
+
+def test_step_linbkg_broad():
+    x = np.linspace(0, 10, 100)
+    center = 5.0
+    sigma = 2.0
+    back0 = 1.0
+    back1 = 0.5
+    dos0 = 2.0
+    dos1 = 1.0
+    expected_result = (back0 + back1 * x) + (dos0 - back0 + (dos1 - back1) * x) * (
+        step_broad(x, center, sigma, 1.0)
+    )
+    assert np.allclose(
+        step_linbkg_broad(x, center, sigma, back0, back1, dos0, dos1), expected_result
+    )
