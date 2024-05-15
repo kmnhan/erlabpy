cc35955e2868d71fd522024c18d7e9cb168f98e1
Kimoon Han <khan@kaist.ac.kr>
2024-05-16 05:11:31 +0900

    test: add tests across multiple modules

--- tests/analysis/test_gold.py
+++ tests/analysis/test_gold.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-from erlab.analysis.gold import poly, spline
+from erlab.analysis.gold import correct_with_edge, poly, spline
 from erlab.io.exampledata import generate_gold_edge
 from numpy.testing import assert_allclose
 
@@ -23,7 +23,7 @@ def test_poly(gold, parallel_kw: dict, fast: bool):
         fast=fast,
         vary_temp=False,
         degree=2,
-        plot=False,
+        plot=True,
         parallel_kw=parallel_kw,
     )
 
@@ -32,6 +32,9 @@ def test_poly(gold, parallel_kw: dict, fast: bool):
         np.array([0.04, 1e-5, -3e-4]),
         atol=1e-2,
     )
+
+    correct_with_edge(gold, res, shift_coords=True, plot=False)
+    correct_with_edge(gold, res, shift_coords=False, plot=False)
 
 
 def test_spline(gold):
@@ -43,7 +46,10 @@ def test_spline(gold):
         vary_temp=False,
         fast=True,
         lam=None,
-        plot=False,
+        plot=True,
     )
 
     assert_allclose(spl(0.0, 0.0), 0.04, rtol=1e-4)
+
+    correct_with_edge(gold, spl, shift_coords=True, plot=False)
+    correct_with_edge(gold, spl, shift_coords=False, plot=True)
--- tests/interactive/test_imagetool.py
+++ tests/interactive/test_imagetool.py
@@ -1,33 +1,38 @@
 import numpy as np
+import pytest
 import xarray as xr
 from erlab.interactive.imagetool import itool
 from numpy.testing import assert_almost_equal
 from qtpy import QtCore
 
 
-def move_and_compare_values(qtbot, win, expected, cursor=0):
+def move_and_compare_values(qtbot, win, expected, cursor=0, target_win=None):
+    if target_win is None:
+        target_win = win
     assert_almost_equal(win.array_slicer.point_value(cursor), expected[0])
 
     # Move left
     qtbot.keyClick(
-        win, QtCore.Qt.Key.Key_Left, QtCore.Qt.KeyboardModifier.ShiftModifier
+        target_win, QtCore.Qt.Key.Key_Left, QtCore.Qt.KeyboardModifier.ShiftModifier
     )
     assert_almost_equal(win.array_slicer.point_value(cursor), expected[1])
 
     # Move down
     qtbot.keyClick(
-        win, QtCore.Qt.Key.Key_Down, QtCore.Qt.KeyboardModifier.ShiftModifier
+        target_win, QtCore.Qt.Key.Key_Down, QtCore.Qt.KeyboardModifier.ShiftModifier
     )
     assert_almost_equal(win.array_slicer.point_value(cursor), expected[2])
 
     # Move right
     qtbot.keyClick(
-        win, QtCore.Qt.Key.Key_Right, QtCore.Qt.KeyboardModifier.ShiftModifier
+        target_win, QtCore.Qt.Key.Key_Right, QtCore.Qt.KeyboardModifier.ShiftModifier
     )
     assert_almost_equal(win.array_slicer.point_value(cursor), expected[3])
 
     # Move up
-    qtbot.keyClick(win, QtCore.Qt.Key.Key_Up, QtCore.Qt.KeyboardModifier.ShiftModifier)
+    qtbot.keyClick(
+        target_win, QtCore.Qt.Key.Key_Up, QtCore.Qt.KeyboardModifier.ShiftModifier
+    )
     assert_almost_equal(win.array_slicer.point_value(cursor), expected[0])
 
 
@@ -43,6 +48,9 @@ def test_itool(qtbot):
 
     move_and_compare_values(qtbot, win, [12.0, 7.0, 6.0, 11.0])
 
+    # Snap
+    qtbot.keyClick(win, QtCore.Qt.Key.Key_S)
+
     # Transpose
     qtbot.keyClick(win, QtCore.Qt.Key.Key_T)
     move_and_compare_values(qtbot, win, [12.0, 11.0, 6.0, 7.0])
@@ -52,4 +60,71 @@ def test_itool(qtbot):
     win.array_slicer.set_bin(0, 1, 2, update=True)
     move_and_compare_values(qtbot, win, [9.0, 8.0, 3.0, 4.0])
 
-    win.slicer_area.set_colormap("viridis", gamma=1.5)
+    # Set colormap and gamma
+    win.slicer_area.set_colormap(
+        "ColdWarm", gamma=1.5, reversed=True, highContrast=True, zeroCentered=True
+    )
+
+    # Lock levels
+    win.slicer_area.lock_levels(True)
+    win.slicer_area.lock_levels(False)
+
+    # Add and remove cursor
+    win.slicer_area.add_cursor()
+    win.slicer_area.remove_current_cursor()
+
+
+def test_value_update(qtbot):
+    data = xr.DataArray(np.arange(25).reshape((5, 5)), dims=["x", "y"])
+    win = itool(data, execute=False)
+    qtbot.addWidget(win)
+    new_vals = -np.arange(25).reshape((5, 5)).astype(float)
+    win.slicer_area.update_values(new_vals)
+    assert_almost_equal(win.array_slicer.point_value(0), -12.0)
+
+    with pytest.raises(ValueError, match="DataArray dimensions do not match"):
+        win.slicer_area.update_values(
+            xr.DataArray(np.arange(24).reshape((2, 2, 6)), dims=["x", "y", "z"])
+        )
+    with pytest.raises(ValueError, match="DataArray dimensions do not match"):
+        win.slicer_area.update_values(
+            xr.DataArray(np.arange(25).reshape((5, 5)), dims=["x", "z"])
+        )
+    with pytest.raises(ValueError, match="DataArray shape does not match"):
+        win.slicer_area.update_values(
+            xr.DataArray(np.arange(24).reshape((4, 6)), dims=["x", "y"])
+        )
+    with pytest.raises(ValueError, match="^Data shape does not match.*"):
+        win.slicer_area.update_values(np.arange(24).reshape((4, 6)))
+
+
+def test_sync(qtbot):
+    data = xr.DataArray(np.arange(25).reshape((5, 5)), dims=["x", "y"])
+    win = itool([data, data], link=True, execute=False)
+    for w in win:
+        qtbot.addWidget(w)
+
+        with qtbot.waitActive(w):
+            w.show()
+            w.activateWindow()
+            w.raise_()
+
+    move_and_compare_values(qtbot, win[1], [12.0, 7.0, 6.0, 11.0], target_win=win[0])
+
+    # Transpose
+    qtbot.keyClick(win[0], QtCore.Qt.Key.Key_T)
+    move_and_compare_values(qtbot, win[1], [12.0, 11.0, 6.0, 7.0], target_win=win[0])
+
+    # Set bin
+    win[0].slicer_area.set_bin(0, 2, update=False)
+    win[0].slicer_area.set_bin(1, 2, update=True)
+
+    # Set all bins, same effect as above since we only have 1 cursor
+    win[0].slicer_area.set_bin_all(1, 2, update=True)
+
+    move_and_compare_values(qtbot, win[1], [9.0, 8.0, 3.0, 4.0], target_win=win[0])
+
+    win[0].slicer_area.set_colormap("ColdWarm", gamma=1.5)
+    assert (
+        win[1].slicer_area.colormap_properties == win[0].slicer_area.colormap_properties
+    )
