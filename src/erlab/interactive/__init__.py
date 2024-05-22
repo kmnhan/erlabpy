560615bb89d2646965d1a2a967133f0df08e3f6e
Kimoon Han <khan@kaist.ac.kr>
2024-05-22 17:19:59 +0900

    refactor(interactive): add informative error message for missing Qt bindings

--- src/erlab/interactive/__init__.py
+++ src/erlab/interactive/__init__.py
@@ -23,6 +23,15 @@ Interactive tools
 
 __all__ = ["dtool", "goldtool", "itool", "ktool"]
 
+
+try:
+    import qtpy  # noqa: F401
+except ImportError as e:
+    raise ImportError(
+        "A Qt binding is required for interactive tools. "
+        "Please install PySide6 or PyQt6"
+    ) from e
+
 from erlab.interactive.derivative import dtool
 from erlab.interactive.fermiedge import goldtool
 from erlab.interactive.imagetool import itool
