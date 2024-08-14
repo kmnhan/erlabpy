4b0cc3c9582476180276854c50109f55e066d3fa
Kimoon Han <khan@kaist.ac.kr>
2024-08-14 23:50:02 +0900

    test: add tests for accessors

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,29 @@
+import lmfit
+import numpy as np
 import pytest
+import xarray as xr
 from erlab.io.exampledata import generate_data_angles, generate_gold_edge
+
+
+def exp_decay(t, n0, tau=1):
+    return n0 * np.exp(-t / tau)
+
+
+@pytest.fixture()
+def exp_decay_model():
+    return lmfit.Model(exp_decay)
+
+
+@pytest.fixture()
+def fit_test_darr():
+    t = np.arange(0, 5, 0.5)
+    da = xr.DataArray(
+        np.stack([exp_decay(t, 3, 3), exp_decay(t, 5, 4), np.nan * t], axis=-1),
+        dims=("t", "x"),
+        coords={"t": t, "x": [0, 1, 2]},
+    )
+    da[0, 0] = np.nan
+    return da
 
 
 @pytest.fixture()
