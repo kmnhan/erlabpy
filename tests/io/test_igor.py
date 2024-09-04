6647a60708538c0a976af2f5312cf4ef6075185f
Kimoon Han <khan@kaist.ac.kr>
2024-09-04 22:12:22 +0900

    test: add basic test for `.pxt` files

--- tests/io/test_igor.py
+++ tests/io/test_igor.py
@@ -12,6 +12,7 @@ WAVE0 = xr.DataArray(
 
 def test_backend(datadir):
     xarray.testing.assert_equal(xr.load_dataarray(datadir / "wave0.ibw"), WAVE0)
+    xarray.testing.assert_equal(xr.load_dataset(datadir / "exp0.pxt")["wave0"], WAVE0)
 
 
 def test_load_wave(datadir):
