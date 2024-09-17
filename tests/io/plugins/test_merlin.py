6f967d86d8febdc077ed7059a56d1328a276083c
Kimoon Han <khan@kaist.ac.kr>
2024-09-17 19:44:10 +0900

    test: add tests for merlin livexy and livepolar

--- tests/io/plugins/test_merlin.py
+++ tests/io/plugins/test_merlin.py
@@ -25,9 +25,21 @@ def test_load_xps(expected_dir):
 
 def test_load_multiple(expected_dir):
     xr.testing.assert_allclose(
+        erlab.io.load("f_005_S001.pxt"),
+        xr.load_dataarray(expected_dir / "5.nc"),
+    )
+    xr.testing.assert_allclose(
         erlab.io.load(5),
         xr.load_dataarray(expected_dir / "5.nc"),
     )
+
+
+def test_load_live(expected_dir):
+    for live in ("lp", "lxy"):
+        xr.testing.assert_allclose(
+            erlab.io.load(f"{live}.ibw"),
+            xr.load_dataarray(expected_dir / f"{live}.nc"),
+        )
 
 
 def test_summarize(data_dir):
