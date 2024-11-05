8d770bfe298253c020aeda6d61a9eab625facf6c
Kimoon Han <khan@kaist.ac.kr>
2024-11-05 22:35:38 +0900

    refactor: remove deprecated module `erlab.characterization`
    BREAKING CHANGE: Deprecated module `erlab.characterization` is removed. Use `erlab.io.characterization` instead.

--- src/erlab/characterization/__init__.py
+++
@@ -1,9 +0,0 @@
-import warnings
-
-from erlab.io.characterization import resistance, xrd  # noqa: F401
-
-warnings.warn(
-    "`erlab.characterization` is deprecated. Use `erlab.io.characterization` instead",
-    DeprecationWarning,
-    stacklevel=2,
-)
