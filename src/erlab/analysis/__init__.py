305832b7bb18aa3d1fda21f4cd0c0992b174d639
Kimoon Han <khan@kaist.ac.kr>
2024-11-05 22:30:40 +0900

    refactor(analysis): remove deprecated alias
    BREAKING CHANGE: Deprecated alias `slice_along_path` in `erlab.analysis` is removed. Call from `erlab.analysis.interpolate` instead.

--- src/erlab/analysis/__init__.py
+++ src/erlab/analysis/__init__.py
@@ -20,18 +20,4 @@ Modules
 
 """
 
-import warnings
-
 from erlab.analysis import fit, gold, image, interpolate, mask, transform  # noqa: F401
-
-
-def slice_along_path(*args, **kwargs):
-    from erlab.analysis.interpolate import slice_along_path
-
-    warnings.warn(
-        "importing as erlab.analysis.slice_along_path is deprecated, "
-        "use erlab.analysis.interpolate.slice_along_path instead",
-        DeprecationWarning,
-        stacklevel=2,
-    )
-    return slice_along_path(*args, **kwargs)
