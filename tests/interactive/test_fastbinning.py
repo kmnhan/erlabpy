d271357530ae35c175bb858f2844627bae940129
Kimoon Han <khan@kaist.ac.kr>
2024-11-12 14:41:40 +0900

    test: move fastbinning tests to interactive

--- tests/interactive/test_fastbinning.py
+++ tests/interactive/test_fastbinning.py
@@ -6,19 +6,21 @@ from erlab.interactive.imagetool.fastbinning import NANMEAN_FUNCS, fast_nanmean
 def test_fast_nanmean():
     for nd, funcs in NANMEAN_FUNCS.items():
         x = np.random.RandomState(42).randn(*((10,) * nd))
-        x64 = x.astype(np.float64)
-        x32 = x.astype(np.float32)
+        x_double = x.astype(np.float64)
+        x_single = x.astype(np.float32)
         for axis, func in funcs.items():
             if isinstance(axis, frozenset):
                 axis = tuple(axis)
-            if not np.allclose(np.nanmean(x64, axis), fast_nanmean(x64, axis)):
+            if not np.allclose(
+                np.nanmean(x_double, axis), fast_nanmean(x_double, axis)
+            ):
                 raise AssertionError(
                     f"fast_nanmean failed for {nd}D float64 array "
                     f"with axis {axis} using {func}."
                 )
             if not np.allclose(
-                np.nanmean(x32.astype(np.float64), axis).astype(np.float32),
-                fast_nanmean(x32, axis),
+                np.nanmean(x_single.astype(np.float64), axis).astype(np.float32),
+                fast_nanmean(x_single, axis),
             ):
                 raise AssertionError(
                     f"fast_nanmean failed for {nd}D float32 array "
