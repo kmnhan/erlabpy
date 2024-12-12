e877e12360157e8303ec0177e8a8999b4c307ab4
Kimoon Han <khan@kaist.ac.kr>
2024-12-12 20:45:32 +0900

    refactor(analysis.fit): enable lazy loading for fit functions

---
+++ src/erlab/analysis/fit/__init__.pyi
@@ -0,0 +1,3 @@
+__all__ = ["functions", "minuit", "models", "spline"]
+
+from . import functions, minuit, models, spline
--- src/erlab/analysis/fit/functions/__init__.py
+++ src/erlab/analysis/fit/functions/__init__.py
@@ -13,44 +13,6 @@ Modules
 
 """
 
-__all__ = [
-    "TINY",
-    "FermiEdge2dFunction",
-    "MultiPeakFunction",
-    "PolynomialFunction",
-    "bcs_gap",
-    "do_convolve",
-    "do_convolve_2d",
-    "dynes",
-    "fermi_dirac",
-    "fermi_dirac_linbkg",
-    "fermi_dirac_linbkg_broad",
-    "gaussian",
-    "gaussian_wh",
-    "lorentzian",
-    "lorentzian_wh",
-    "step_broad",
-    "step_linbkg_broad",
-]
+import lazy_loader as _lazy
 
-from erlab.analysis.fit.functions.dynamic import (
-    FermiEdge2dFunction,
-    MultiPeakFunction,
-    PolynomialFunction,
-)
-from erlab.analysis.fit.functions.general import (
-    TINY,
-    bcs_gap,
-    do_convolve,
-    do_convolve_2d,
-    dynes,
-    fermi_dirac,
-    fermi_dirac_linbkg,
-    fermi_dirac_linbkg_broad,
-    gaussian,
-    gaussian_wh,
-    lorentzian,
-    lorentzian_wh,
-    step_broad,
-    step_linbkg_broad,
-)
+__getattr__, __dir__, __all__ = _lazy.attach_stub(__name__, __file__)
