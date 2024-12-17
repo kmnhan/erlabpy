0b14f750973b0dc897536a93ef50bfbdb1af216b
Kimoon Han <khan@kaist.ac.kr>
2024-12-17 19:41:57 +0900

    fix(io.dataloader): assign coords also for multifile scan of length 1

--- src/erlab/io/dataloader.py
+++ src/erlab/io/dataloader.py
@@ -547,7 +547,7 @@ class LoaderBase(metaclass=_Loader):
             )  # Return type enforced by metaclass, cast to avoid mypy error
             # file_paths: list of file paths with at least one element
 
-            if len(file_paths) == 1:
+            if len(coord_dict) == 0:
                 # Single file resolved
                 data: xr.DataArray | xr.Dataset | xr.DataTree = self.load_single(
                     file_paths[0], **load_kwargs
