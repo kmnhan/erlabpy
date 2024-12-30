9cabab20d90549c15a9f99e8a5db0628b04e7f13
Kimoon Han <khan@kaist.ac.kr>
2024-12-30 10:19:50 +0900

    chore: move server functionality to _server module

--- src/erlab/interactive/imagetool/manager/__init__.py
+++ src/erlab/interactive/imagetool/manager/__init__.py
@@ -22,7 +22,6 @@ __all__ = [
     "show_in_manager",
 ]
 
-import contextlib
 import datetime
 import enum
 import gc
@@ -35,8 +34,6 @@ import socket
 import struct
 import sys
 import tempfile
-import threading
-import time
 import uuid
 import weakref
 from collections.abc import Hashable, Iterable, ValuesView
@@ -58,10 +55,14 @@ from erlab.interactive.imagetool.manager._dialogs import (
     _RenameDialog,
     _StoreDialog,
 )
+from erlab.interactive.imagetool.manager._server import (
+    PORT,
+    _ManagerServer,
+    _save_pickle,
+)
 from erlab.interactive.utils import (
     IconActionButton,
     KeyboardEventFilter,
-    _coverage_resolve_trace,
     file_loaders,
     wait_dialog,
 )
@@ -78,12 +79,6 @@ if TYPE_CHECKING:
 
 logger = logging.getLogger(__name__)
 
-PORT: int = int(os.getenv("ITOOL_MANAGER_PORT", "45555"))
-"""Port number for the manager server.
-
-The default port number 45555 can be overridden by setting the environment variable
-``ITOOL_MANAGER_PORT``.
-"""
 
 _SHM_NAME: str = "__enforce_single_itoolmanager"
 """Name of `QtCore.QSharedMemory` that enforces single instance of ImageToolManager.
@@ -124,24 +119,6 @@ _always_use_socket: bool = False
 
 class _WrapperItemDataRole(enum.IntEnum):
     ToolIndexRole = QtCore.Qt.ItemDataRole.UserRole + 1
-
-
-def _save_pickle(obj: Any, filename: str) -> None:
-    with open(filename, "wb") as file:
-        pickle.dump(obj, file, protocol=-1)
-
-
-def _load_pickle(filename: str) -> Any:
-    with open(filename, "rb") as file:
-        return pickle.load(file)
-
-
-def _recv_all(conn, size):
-    data = b""
-    while len(data) < size:
-        part = conn.recv(size - len(data))
-        data += part
-    return data
 
 
 def _format_dim_name(s: Hashable) -> str:
@@ -271,71 +248,6 @@ def _fill_rounded_rect(
 
 class ItoolManagerParseError(Exception):
     """Raised when the data received from the client cannot be parsed."""
-
-
-class _ManagerServer(QtCore.QThread):
-    sigReceived = QtCore.Signal(list, dict)
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.stopped = threading.Event()
-
-    @_coverage_resolve_trace
-    def run(self) -> None:
-        self.stopped.clear()
-
-        logger.debug("Starting server...")
-        soc = socket.socket()
-        soc.bind(("127.0.0.1", PORT))
-        soc.setblocking(False)
-        soc.listen()
-
-        logger.info("Server is listening...")
-
-        while not self.stopped.is_set():
-            try:
-                conn, _ = soc.accept()
-            except BlockingIOError:
-                time.sleep(0.01)
-                continue
-
-            conn.setblocking(True)
-            logger.debug("Connection accepted")
-            # Receive the size of the data first
-            data_size = struct.unpack(">L", _recv_all(conn, 4))[0]
-
-            # Receive the data
-            kwargs = _recv_all(conn, data_size)
-            try:
-                kwargs = pickle.loads(kwargs)
-                logger.debug("Received data: %s", kwargs)
-
-                files = kwargs.pop("__filename")
-                self.sigReceived.emit([_load_pickle(f) for f in files], kwargs)
-                logger.debug("Emitted loaded data")
-
-                # Clean up temporary files
-                for f in files:
-                    os.remove(f)
-                    dirname = os.path.dirname(f)
-                    if os.path.isdir(dirname):
-                        with contextlib.suppress(OSError):
-                            os.rmdir(dirname)
-                logger.debug("Cleaned up temporary files")
-
-            except (
-                pickle.UnpicklingError,
-                AttributeError,
-                EOFError,
-                ImportError,
-                IndexError,
-            ):
-                logger.exception("Failed to unpickle received data")
-
-            conn.close()
-            logger.debug("Connection closed")
-
-        soc.close()
 
 
 class _ImageToolWrapper(QtCore.QObject):
