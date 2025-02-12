82080b54508e009d4d9368ada8df09b4742597ad
Kimoon Han <khan@kaist.ac.kr>
2025-02-12 09:50:44 +0900

    docs: add hv-dependent conversion example

--- tests/io/test_exampledata.py
+++ tests/io/test_exampledata.py
@@ -1,6 +1,11 @@
 import numpy as np
 
-from erlab.io.exampledata import generate_data, generate_data_angles, generate_gold_edge
+from erlab.io.exampledata import (
+    generate_data,
+    generate_data_angles,
+    generate_gold_edge,
+    generate_hvdep_cuts,
+)
 
 
 def test_generate_data() -> None:
@@ -88,4 +93,35 @@ def test_generate_gold_edge() -> None:
     )
 
     np.testing.assert_allclose(data.eV.values, np.array([-1.3, -0.9, -0.5, -0.1, 0.3]))
+    np.testing.assert_allclose(data.alpha.values, np.array([-15.0, 0.0, 15.0]))
+
+
+def test_generate_hvdep_cuts() -> None:
+    data = generate_hvdep_cuts((3, 3, 3), noise=False, temp=0, hvrange=(40, 60))
+
+    np.testing.assert_allclose(
+        data.values,
+        np.array(
+            [
+                [
+                    [79.90509526, 46.07895401, 50.66075799],
+                    [30.47582358, 29.52025701, 34.92976538],
+                    [51.82761664, 43.65856414, 47.96927155],
+                ],
+                [
+                    [100.26801478, 143.17916656, 195.53833693],
+                    [35.33446342, 50.39667113, 68.76074904],
+                    [18.00410008, 25.62919158, 34.91833759],
+                ],
+                [
+                    [79.90509526, 46.07895401, 50.66075799],
+                    [30.47582358, 29.52025701, 34.92976538],
+                    [51.82761664, 43.65856414, 47.96927155],
+                ],
+            ]
+        ),
+    )
+
+    np.testing.assert_allclose(data.eV.values, np.array([-0.45, -0.165, 0.12]))
+    np.testing.assert_allclose(data.hv.values, np.array([40, 50, 60]))
     np.testing.assert_allclose(data.alpha.values, np.array([-15.0, 0.0, 15.0]))
