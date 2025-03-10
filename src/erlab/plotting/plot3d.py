89c157248a4d1bafd776b57e3534c73892087008
Kimoon Han <khan@kaist.ac.kr>
2025-03-10 10:39:22 +0900

    Revert part of "refactor: move fitting accessors to `xarray-lmfit` package"
    This reverts part of commit 404494bc4dffd0f9941cef3d8804408ed818e58b.

--- src/erlab/plotting/plot3d.py
+++ src/erlab/plotting/plot3d.py
@@ -1,7 +1,5 @@
 """Extensions to mplot3d."""
 
-import contextlib
-
 import matplotlib.collections
 import matplotlib.patches
 import mpl_toolkits.mplot3d
@@ -43,7 +41,10 @@ class FancyArrowPatch3D(matplotlib.patches.FancyArrowPatch):
 # improved from https://stackoverflow.com/q/18228966
 
 
-def _transform_zdir(zn):
+def _transform_zdir(zdir):
+    zdir = mpl_toolkits.mplot3d.art3d.get_dir_vector(zdir)
+    zn = zdir / np.linalg.norm(zdir)
+
     cos_angle = zn[2]
     sin_angle = np.linalg.norm(zn[:2])
     if sin_angle == 0:
@@ -56,58 +57,28 @@ def _transform_zdir(zn):
     return ddt + cos_angle * (np.eye(3) - ddt) + sin_angle * skew
 
 
-def _set_arbitrary_3d_properties(
-    self, verts, zs=0, zdir="z", axlim_clip: bool = False
-) -> None:
-    """
-    Set the *z* position and direction of the patch.
-
-    Parameters
-    ----------
-    verts :
-        The vertices of the patch.
-    zs : float
-        The location along the *zdir* axis in 3D space to position the
-        patch.
-    zdir : {'x', 'y', 'z'} or 3-element array-like, optional
-        Plane to plot patch orthogonal to. Default: 'z'.
-    axlim_clip : bool, default: False
-        Whether to hide patches with a vertex outside the axes view limits.
-    """
+def set_3d_properties(self, verts, zs=0, zdir="z") -> None:
     zs = np.broadcast_to(zs, len(verts))
-    zn = zdir = mpl_toolkits.mplot3d.art3d.get_dir_vector(zdir)
-    zn = zdir / np.linalg.norm(zdir)
     self._segment3d = np.asarray(
         [
-            np.dot(_transform_zdir(zn), (x, y, 0.0)) + zn * z
+            (*np.dot(_transform_zdir(zdir), (x, y, 0)), 0, 0, z)
             for ((x, y), z) in zip(verts, zs, strict=True)
         ]
     )
-    self._axlim_clip = axlim_clip
 
 
-def _translate_patch(pathpatch, delta) -> None:
+def pathpatch_translate(pathpatch, delta) -> None:
     pathpatch._segment3d += np.asarray(delta)
 
 
-def to_3d(
-    patch: matplotlib.patches.Patch, z=0.0, zdir="z", delta=(0, 0, 0)
-) -> matplotlib.patches.Patch:
-    if not hasattr(patch.axes, "get_zlim"):
+def to_3d(pathpatch, z=0.0, zdir="z", delta=(0, 0, 0)):
+    if not hasattr(pathpatch.axes, "get_zlim"):
         raise ValueError("Axes projection must be 3D")
-    mpl_toolkits.mplot3d.art3d.pathpatch_2d_to_3d(patch, z=z, zdir=zdir)
-    _translate_patch(patch, delta)
-    return patch
+    mpl_toolkits.mplot3d.art3d.pathpatch_2d_to_3d(pathpatch, z=z, zdir=zdir)
+    pathpatch.translate(delta)
+    return pathpatch
 
 
-@contextlib.contextmanager
-def patch3d_context():
-    _set_3d_properties = mpl_toolkits.mplot3d.art3d.Patch3D.set_3d_properties
-    mpl_toolkits.mplot3d.art3d.Patch3D.set_3d_properties = _set_arbitrary_3d_properties
-    matplotlib.patches.Patch.to_3d = to_3d
-
-    try:
-        yield
-    finally:
-        mpl_toolkits.mplot3d.art3d.Patch3D.set_3d_properties = _set_3d_properties
-        del matplotlib.patches.Patch.to_3d
+# mpl_toolkits.mplot3d.art3d.Patch3D.set_3d_properties = set_3d_properties
+mpl_toolkits.mplot3d.art3d.Patch3D.translate = pathpatch_translate
+# matplotlib.patches.Patch.to_3d = to_3d
