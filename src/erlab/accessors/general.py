fix(qsel): preserve non-averaged coord in multidimensional associated coordinates
Fixes an issue where averaging over a dimension with `DataArray.qsel()` or `DataArray.qsel.average()` with multidimensional associated coordinates would average the coordinates over all dimensions instead of averaging over just the specified dimension.

src/erlab/accessors/general.py
@@ -485,7 +485,7 @@ class SelectionAccessor(ERLabDataArrayAccessor):
             out = out.sel(slices)
 
             lost_coords = {
-                k: out[k].mean(keep_attrs=True)
+                k: out[k].mean(dim=avg_dims, keep_attrs=True)
                 for k in lost_dims
                 if k not in unindexed_dims
             }