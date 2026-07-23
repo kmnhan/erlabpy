64e35ff0675a27928e0de3afc49316cb9e64a9e9
Kimoon Higashihira Han <98246499+kmnhan@users.noreply.github.com>
2026-07-23 20:12:55 +0900

    chore: revert kriss changes

--- src/erlab/io/plugins/kriss.py
+++ src/erlab/io/plugins/kriss.py
@@ -15,9 +15,5 @@ class KRISSLoader(DA30Loader):
     additional_attrs: typing.ClassVar[dict] = {"configuration": 4}
 
     @property
-    def file_dialog_methods(self):
-        return {"KRISS Data (*.ibw *.pxt *.zip)": (self.load, {})}
-
-    @property
     def name_map(self):
         return super().name_map | {"chi": "R1", "xi": ["R2", "Point [deg]"]}
